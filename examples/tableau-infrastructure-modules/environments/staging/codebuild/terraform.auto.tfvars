e9e1a423ef6a7948869902a23f5c616a161e991e
Andy Singleton <asing@brightprocess.com>
2021-11-25 14:25:59 +0000

    OTAB-69: Update codebuild module structure to fit new module release

--- examples/tableau-infrastructure-modules/environments/staging/codebuild/terraform.auto.tfvars
+++ examples/tableau-infrastructure-modules/environments/staging/codebuild/terraform.auto.tfvars
@@ -1,21 +1,20 @@
-# Account
+# Account and general
 account_alias = "<FRIENDLY_NAME_FOR_THIS_ACCOUNT>"
 aws_region    = "<YOUR_REGION>"
-common_tags   = {
-  Application  = "Tableau"
-  Element      = "Codebuild project"
-  Purpose      = "Build Tableau AMIs"
-  Tool         = "Terraform"
+common_tags = {
+  Application = "Tableau"
+  Element     = "CodeBuild"
+  Purpose     = "Building of Tableau AMIs"
+  Tool        = "Terraform"
 }
 
 # VPC configuration
-vpc_filter            = "<NAME OF PARTIAL NAME OF YOUR VPC>"
-private_subnet_filter = "<FILTER FOR BUILD SUBNET NAME>"
+vpc_name           = "<NAME OF YOUR VPC>"
+subnet_name_filter = "<FILTER FOR BUILD SUBNET NAME>"
 
 # Project configuration
-encrypt_ami               = true
-environment               = "<YOUR_ENVIRONMENT_NAME>"
-packer_file_location      = "ami/packfiles/amazon.json"
-packer_vars_file_location = "ami/environment/default.json"
-project_name              = "tableau-ami"
-source_repository_url     = "https://github.com/osodevops/aws-terraform-module-tableau-public"
+encrypt_ami           = true
+kms_key_arn           = ""
+environment           = "<YOUR_ENVIRONMENT_NAME>"
+project_name          = "tableau-ami"
+source_repository_url = "<URL OF THE AMI BUILD REPO>"
--- examples/tableau-infrastructure-modules/environments/staging/codebuild/variables.tf
+++ examples/tableau-infrastructure-modules/environments/staging/codebuild/variables.tf
@@ -2,39 +2,86 @@ variable "account_alias" {
   description = "Name of the account"
   type        = string
 }
+
+variable "additional_build_variables" {
+  description = "A map of key-values that will be defined as environment variables in the codebuild project"
+  type        = map(string)
+  default     = {}
+}
+
+variable "ami_post_build_commands" {
+  description = "Override the default ami post-build commands"
+  type        = list(string)
+  default     = []
+}
+
 variable "aws_region" {
   description = "The AWS region in which all resources will be created"
   type        = string
 }
+
 variable "environment" {
   description = "Single prefix of account, examples: M"
   type        = string
 }
+
 variable "encrypt_ami" {
   description = "Encrypt AMI after successful build."
   type        = string
 }
+
+variable "instance_type" {
+  description = "Instance type used by packer to build"
+  type        = string
+  default     = "m5a.2xlarge"
+}
+
 variable "kms_key_arn" {
   description = "If Encrypt_ami set to true then you must pass in the arn of the key you wish to encrypt disk with."
   type        = string
   default     = "Default"
 }
+
 variable "packer_file_location" {
   description = "The file path of the .json packer to build."
   type        = string
+  default     = "ami/tableau/packer/packfile.json"
 }
-variable "packer_vars_file_location" {
-  description = "The file path to where extra Packer vars can be referenced"
-  type        = string
-}
+
 variable "project_name" {
   description = "Name to give the CodeBuild Project"
   type        = string
 }
+
+variable "root_volume_size" {
+  description = "Specify the root volume size for the built image"
+  type        = string
+  default     = "150"
+}
+
+variable "shared_ami_users" {
+  description = "List of user accounts to share the built AMI with"
+  type        = string
+  default     = ""
+}
+
+variable "source_image_account_no" {
+  description = "Account number owning the source image for the packer build"
+  type        = string
+  default     = "amazon"
+}
+
 variable "source_repository_url" {
   description = "The source repository URL of OSO DevOps Tableau module."
   type        = string
 }
+
+variable "ssm_path_github_token" {
+  description = "Path in SSM Parameter Store of the github OAUTH token"
+  type        = string
+  default     = ""
+}
+
 variable "common_tags" {
   description = "A collection of common tags to be added to resources."
   type        = map(string)
@@ -45,10 +92,10 @@ variable "vpc_name" {
   type        = string
 }
 
-variable "private_subnet_filter" {
+variable "subnet_name_filter" {
   description = "Tag value used to filter for private subnets we will be building in"
   type        = string
-  default     = "tableau-build"
+  default     = "Private*"
 }
 
 locals {
